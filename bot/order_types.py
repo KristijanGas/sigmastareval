11167aa15b5ab538b4206c3a0703b1cc02ed3db6
VitoLjubotina <vl55536@fer.hr>
2026-07-02 14:56:20 +0200

    order actions

--- bot/order_types.py
+++ bot/order_types.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
-class OrderType(Enum):
-    GTC = "GTC"   #Good Till Cancelled
-    GTD = "GTD"   #Good Till Date
-    FOK = "FOK"   #Fill Or Kill
-    FAK = "FAK"   #Fill And Kill
+class OrderType(str, Enum):
+   GTC = "GTC"   #Good Till Cancelled
+   GTD = "GTD"   #Good Till Date
+   FOK = "FOK"   #Fill Or Kill
+   FAK = "FAK"   #Fill And Kill
