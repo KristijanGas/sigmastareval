00edc846e74dedabc52248c905425b60ea8861fc
KristijanGas <gaspar.kristijan@gmail.com>
2026-07-15 00:58:08 +0200

    new data

--- data/data_scraper_master.py
+++ data/data_scraper_master.py
@@ -8,7 +8,7 @@ import time
 #https://clob.polymarket.com/book?token_id=54723568072009946861830956098453721516917366403655545781627131273815785194717 # token moras izvadit iz ovog prvog i onda koristit
 
 #https://gamma-api.polymarket.com/markets?closed=false&limit=1000
-#https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT
+#
 
 
 markets = [("bitcoin-up-or-down","BTCUSDT", "hourly"),("ethereum-up-or-down","ETHUSDT", "hourly"),
@@ -28,7 +28,7 @@ try:
 
         print("Updating metadata...")
         subprocess.run([sys.executable, "data/data_meta_fill.py"], check=True)
-        time.sleep(2000)
+        time.sleep(5000)
 except KeyboardInterrupt:
     print("Terminating all scraper processes...")
     for process in processes:
