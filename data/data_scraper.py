330c97f3abb892f2e2d8246ee5f2f5a3ec13fd50
Your Name <kristijan.gaspar@fer.unizg.hr>
2026-07-14 02:56:44 +0200

    websocket for pulling data

--- data/data_scraper.py
+++ data/data_scraper.py
@@ -4,10 +4,13 @@ import time
 import urllib.request
 import urllib.parse
 import json
+from websocket import WebSocketApp
 from data_interface import parse_time_name_5m, parse_time_name_hourly
 from datetime import datetime
 from zoneinfo import ZoneInfo
 import gzip
+import threading
+
 
 #markets = ["bitcoin-up-or-down"]
 #https://gamma-api.polymarket.com/events?slug=bitcoin-up-or-down-june-30-2026-2pm-et
@@ -41,10 +44,10 @@ def get_current_market_names(time_name,market):
     return market_metadata
 
 def get_clob_data(market_metadata):
-    clob_token_ids = market_metadata[0]["markets"][0]["clobTokenIds"].split(", ")
+    clob_token_ids = json.loads(market_metadata[0]["markets"][0]["clobTokenIds"])
     clobs = []
     for i in range(len(clob_token_ids)):
-        token_id = "".join(c for c in clob_token_ids[i] if c.isdigit())
+        token_id = clob_token_ids[i]
         path = f"https://clob.polymarket.com/book?token_id={token_id}"
         
         request = urllib.request.Request(
@@ -64,37 +67,65 @@ def get_clob_data(market_metadata):
         clobs.append((token_id,market_data))
     return clobs
 
-def get_price_data(market_binance):
-    path = f"https://api.binance.com/api/v3/ticker/price?symbol={market_binance}"
-    request = urllib.request.Request(
-        path,
-        headers={
-            "User-Agent": "Mozilla/5.0",
-            "Accept": "application/json, text/plain, */*",
-            "Referer": "https://polymarket.com/",
-        },
-    )
-    try:
-        with urllib.request.urlopen(request, timeout=20) as url:
-            market_data = json.loads(url.read().decode())
-    except Exception as e:
-        print(f"Error fetching price data for {market_binance}: {e}")
-        market_data = None
 
-    if market_data:
-        timestamp = datetime.now(ZoneInfo("America/New_York")).timestamp()
-        market_data["timestamp"] = timestamp
-    #print(f"Fetched price data for {market_binance}: {market_data}")
-    return market_data
+
+class BinancePriceFeed:
+    def __init__(self, symbol: str):
+        self.symbol = symbol.upper()
+
+        self._lock = threading.Lock()
+        self._buffer = []
+
+    def _on_message(self, ws, message):
+        data = json.loads(message)
+
+        price = (float(data["b"]) + float(data["a"])) / 2
+
+        tick = {
+            "symbol": self.symbol,
+            "price": price,
+            "timestamp": datetime.now(ZoneInfo("America/New_York")).timestamp()
+        }
+        
+        with self._lock:
+            if len(self._buffer) == 0 or price != self._buffer[-1]["price"]:
+                self._buffer.append(tick)
+
+    def start(self):
+        url = (
+            f"wss://stream.binance.com:9443/ws/"
+            f"{self.symbol.lower()}@bookTicker"
+        )
+
+        self.ws = WebSocketApp(
+            url,
+            on_message=self._on_message,
+        )
+        thread = threading.Thread(
+            target=self._run,
+            daemon=True,
+        )
+        thread.start()
+
+    def consume(self):
+        with self._lock:
+            data = self._buffer
+            self._buffer = []
+        return data
+    
+    def _run(self):
+        while True:
+            try:
+                self.ws.run_forever()
+            except Exception as e:
+                print(f"Websocket error: {e}")
+
+            print("Disconnected. Reconnecting in 3 seconds...")
+            time.sleep(3)
 
 def store_data(data, time_name,market):
 
     store_path = f"datasets/{market}/{market}-{time_name}.gz"
-    # create file
-
-    #reading it badck
-    #with gzip.open("data.json.gz", "rt", encoding="utf-8") as f:
-    #    data = json.load(f)
     os.makedirs(os.path.dirname(store_path), exist_ok=True)
     with gzip.open(store_path, "wt", encoding="utf-8") as f:
         json.dump(data, f)
@@ -117,22 +148,21 @@ def __main__():
     print(old_time_name)
     data = {"metadata_start": markets_metadata, "all_clobs": [], "all_prices": [], "metadata_end": None}
     ind = 0
-
+    crypto_value_feed = BinancePriceFeed(market_binance)
+    crypto_value_feed.start()
     while 1:
         if market_type == "hourly":
             time_name = parse_time_name_hourly()["hourly_name"]
         elif market_type == "5m":
             time_name = parse_time_name_5m()
-            print(f"Current 5-minute timestamp: {time_name}")
 
         if (time_name != old_time_name):
             markets_metadata_old = get_current_market_names(old_time_name, market)
             data["metadata_end"] = markets_metadata_old
-            #print(data[market]["metadata_end"])
+            data["all_prices"] = crypto_value_feed.consume()  # Get the latest prices
             store_data(data,old_time_name,market)
 
             #new batch
-
             markets_metadata = get_current_market_names(time_name, market)
             data = {"metadata_start": markets_metadata, "all_clobs": [], "all_prices": [], "metadata_end": None}
 
@@ -143,14 +173,11 @@ def __main__():
 
         clobs = get_clob_data(market_metadata)
         data["all_clobs"].append(clobs)
-        prices = get_price_data(market_binance)
-        data["all_prices"].append(prices)
 
         ind += 1
-        print(time_name)
+        print(time_name, market_binance)
         
 
 
-        time.sleep(0.5)
 
 __main__()
