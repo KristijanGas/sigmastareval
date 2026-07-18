0592426d321306316a91080efc511c9b059a2c95
Your Name <kristijan.gaspar@fer.unizg.hr>
2026-07-18 02:34:45 +0200

    live provider implemented

--- data_provider/live_provider.py
+++ data_provider/live_provider.py
@@ -4,7 +4,9 @@ import sys
 import threading
 import time
 from zoneinfo import ZoneInfo
-from data_provider import historical_provider
+
+import urllib.request
+import urllib.parse
 from pathlib import Path
 
 if __package__ is None or __package__ == "":
@@ -17,10 +19,12 @@ REPO_ROOT = Path(__file__).resolve().parents[1]
 
 from data_provider.binance_price_feed import BinancePriceFeed
 from data.data_interface import parse_time_name_5m, parse_time_name_hourly
+from data_provider.order_book_feed import OrderBookFeed
+from data_provider.historical_provider import historical_provider
 
 
 class live_provider(historical_provider):
-    def __init__(self, binance_symbol, market_slug_base, market_type , market):
+    def __init__(self, market_slug_base, binance_symbol, market_type , market):
         self.binance_feed = BinancePriceFeed(binance_symbol)
         self.binance_feed.on_price_change = self.set_crypto_value
         self.binance_feed.start()
@@ -45,25 +49,60 @@ class live_provider(historical_provider):
                     time_name = parse_time_name_hourly()["hourly_name"]
                 elif self.market_type == "5m":
                     time_name = parse_time_name_5m()
+
                 if self.last_time_name is None or time_name != self.last_time_name:
                     self.last_time_name = time_name
-                    self.order_book = None
+                    self.order_book = []
                     self.price_to_beat = None
                     self.end_timestamp = None
                     self.metadata = None
                     self.up_token_id = None
                     self.down_token_id = None
-                    self.set_market()
-                time.sleep(1)  # Sleep for a second before the next iteration
+                    self.set_market(time_name)
+                
+                self.metadata = self.get_metadata(time_name, self.market_slug_base)
+                if self.metadata[0]["eventMetadata"] is not None and self.metadata[0]["eventMetadata"]["priceToBeat"] is not None:
+                    self.set_price_to_beat(self.metadata[0]["eventMetadata"]["priceToBeat"])
+
+                #time.sleep(0.1)  # Sleep for a second before the next iteration
+                #print(self.get_best_bid(self.up_token_id), self.get_best_bid(self.down_token_id), self.get_best_ask(self.up_token_id), self.get_best_ask(self.down_token_id), self.get_crypto_value(), self.get_price_to_beat(), self.get_current_timestamp(), self.get_end_timestamp())
             except Exception as e:
                 print(f"Error in live_provider run loop: {e}")
                 time.sleep(1)
 
-
-
-    def set_market(self):
-        self.set_price_to_beat(self.get_crypto_value())
+    def get_metadata(self, time_name, market_slug_base):
         
+        #print(f"Fetching market metadata for {market} at {time_name}")
+        full_name = f"{market_slug_base}-{time_name}"
+        path = f"https://gamma-api.polymarket.com/events?slug={full_name}"
+        #print(path)
+        request = urllib.request.Request(
+            path,
+            headers={
+                "User-Agent": "Mozilla/5.0",
+                "Accept": "application/json, text/plain, */*",
+                "Referer": "https://polymarket.com/",
+            },
+        )
+        try: 
+            with urllib.request.urlopen(request, timeout=20) as url:
+                market_metadata = json.loads(url.read().decode())
+        except Exception as e:
+            print(f"Error fetching market metadata for {full_name} at {time_name}: {e}")
+            market_metadata = None
+        return market_metadata
+
+    def set_market(self, time_name):
+        self.set_price_to_beat(self.get_crypto_value())
+        while self.metadata is None:
+            try:
+                self.metadata = self.get_metadata(time_name, self.market_slug_base)
+            except Exception as e:
+                print(f"Error fetching metadata for {self.market_slug_base} at {time_name}: {e}")
+                time.sleep(1)
+
+        self.set_end_timestamp(self.metadata[0]["endDate"])
+
         outcome_name_list = json.loads(self.metadata[0]["markets"][0]["outcomes"])
         self.token_ids = json.loads(self.metadata[0]["markets"][0]["clobTokenIds"])
         for i in range(len(outcome_name_list)):
@@ -71,6 +110,16 @@ class live_provider(historical_provider):
                 self.up_token_id = self.token_ids[i]
             elif outcome_name_list[i] == "Down":
                 self.down_token_id = self.token_ids[i]
+            
+        self.up_feed = OrderBookFeed(self.up_token_id, self.order_book)
+        self.down_feed = OrderBookFeed(self.down_token_id, self.order_book)
+        print(f"Set market with Up token ID: {self.up_token_id}, Down token ID: {self.down_token_id}, End timestamp: {self.end_timestamp}")
+        # start threads
+        self.up_thread = threading.Thread(target=self.up_feed.run, daemon=True)
+        self.down_thread = threading.Thread(target=self.down_feed.run, daemon=True)
+        self.up_thread.start()
+        self.down_thread.start()
+        
 
     def get_current_timestamp(self):
         current_time = datetime.now(ZoneInfo("America/New_York"))
@@ -82,9 +131,6 @@ class live_provider(historical_provider):
     
     def get_crypto_value(self):
         return self.binance_feed.get_current_price()
-
-    def set_order_book(self, order_book):
-        self.order_book = order_book
 
     def set_end_timestamp(self, end_date):
         dt = datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%SZ")
@@ -108,3 +154,13 @@ class live_provider(historical_provider):
                     level["size"] = size
                     break
     
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: python live_provider.py <market_slug_base> <binance_symbol> <market_type>")
+        sys.exit(1)
+    market_slug_base = sys.argv[1]
+    binance_symbol = sys.argv[2]
+    market_type = sys.argv[3]
+    provider = live_provider(market_slug_base, binance_symbol, market_type, None)
+    provider.run()
---
+++ data_provider/order_book_feed.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta
+import json
+import threading
+import time
+from zoneinfo import ZoneInfo
+
+import urllib
+
+class OrderBookFeed:
+    def __init__(self, asset_id, order_book=None):
+        self._lock = threading.Lock()
+        self.asset_id = asset_id
+        self.order_book = order_book
+
+    def query_order_book(self, token_id):
+        path = f"https://clob.polymarket.com/book?token_id={token_id}"
+        #print(f"Fetching order book for token_id {token_id} from {path}")
+        request = urllib.request.Request(
+            path,
+            headers={
+                "User-Agent": "Mozilla/5.0",
+                "Accept": "application/json, text/plain, */*",
+                "Referer": "https://polymarket.com/",
+            },
+        )
+        try:
+            with urllib.request.urlopen(request, timeout=20) as url:
+                market_data = json.loads(url.read().decode())
+        except Exception as e:
+            print(f"Error fetching CLOB data for token_id {token_id}: {e}")
+            market_data = None
+        return (token_id, market_data)
+    
+    def run(self):
+        while True:
+            try:
+                order_book_for_asset = self.query_order_book(self.asset_id)
+            except Exception as e:
+                print(f"Error in OrderBookFeed WebSocket: {e}")
+                time.sleep(5)  # Wait before trying to reconnect
+            found = 0
+
+            for i in range(len(self.order_book)):
+                if self.order_book[i][0] == self.asset_id:
+                    with self._lock:
+                        self.order_book[i] = order_book_for_asset
+                    found = 1
+                    break
+
+            if found == 0:
+                with self._lock:
+                    self.order_book.append((self.asset_id, order_book_for_asset))
