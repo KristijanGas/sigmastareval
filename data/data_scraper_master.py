662c4dd68c92e190a523a10082f7aa961ff4dba7
KristijanGas <gaspar.kristijan@gmail.com>
2026-08-04 15:36:35 +0200

    more proper data from new stream

--- data/data_scraper_master.py
+++ data/data_scraper_master.py
@@ -28,7 +28,7 @@ try:
 
         print("Updating metadata...")
         subprocess.run([sys.executable, "data/data_meta_fill.py"], check=True)
-        time.sleep(5000)
+        time.sleep(3600)
 except KeyboardInterrupt:
     print("Terminating all scraper processes...")
     for process in processes:
